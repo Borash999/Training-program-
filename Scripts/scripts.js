380d67cee91bf53be207b03b23a36450c1886d98
Vlad <vladik.bar4noff@yandex.ru>
2023-04-11 14:25:23 +0800

    Добавили поле ввода и кнопку

---
+++ Scripts/scripts.js
@@ -0,0 +1,9 @@
+function getValue() {
+  var InputValue = document.getElementById('MyInput').value;
+  if (InputValue == "") {
+    alert ("Введите данные");
+  } else  { 
+   alert ("Данные отправлены");
+  }
+
+}
